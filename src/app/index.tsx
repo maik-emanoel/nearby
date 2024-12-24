947d1deca60b8f665ce0cc2ded4f255410f5e91d
Maik Emanoel <maikemanoel11@gmail.com>
2024-12-24 13:21:53 -0300

    feat: add button to navigate to home route

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,13 +1,20 @@
+import { View, StatusBar } from 'react-native'
+import { router } from 'expo-router'
+
+import { Button } from '@/components/button'
 import { Steps } from '@/components/steps'
 import { Welcome } from '@/components/welcome'
-import { View, Text, StatusBar } from 'react-native'
 
 export default function Index() {
   return (
     <View style={{ flex: 1, padding: 40, gap: 40 }}>
       <Welcome />
       <Steps />
-      <StatusBar barStyle='default' />
+
+      <Button.Root onPress={() => router.navigate('/home')}>
+        <Button.Title>Começar</Button.Title>
+      </Button.Root>
+      <StatusBar barStyle="dark-content" />
     </View>
   )
 }
